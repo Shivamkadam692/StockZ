e5d27823514576350ce43933d793b6db5996828a
Shivamkadam692 <shivam.123kadam@gmail.com>
2025-11-25 14:35:44 +0530

    home page created

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -4,7 +4,46 @@ import React from 'react';
 function Awards() {
     return ( 
 
-        <h1>Awards Page</h1>
+     <div className="container">
+         <div className="row">
+             <div className="col-6">
+
+                <img src="media/images/largestBroker.svg" alt="Awards Img" />
+        </div>
+         <div className="col-6">
+
+            <h1>Largest Broker in India</h1>
+            <p>We are proud to be the largest broker in India, serving millions of customers with top-notch services and a wide range of investment options.</p>
+        <div className="row">
+          <div className='col-6'>
+
+                  <ul>
+                <li>
+                    <p>  Extensive range of investment options</p></li>
+                <li><p>Reliable and secure platform</p></li>
+                <li><p>Exceptional customer support</p></li>
+            </ul>
+
+
+          </div>
+                   <div className='col-6'>
+
+                  <ul>
+                <li>
+                    <p>  Extensive range of investment options</p></li>
+                <li><p>Reliable and secure platform</p></li>
+                <li><p>Exceptional customer support</p></li>
+            </ul>
+
+
+          </div>
+          <img src="media/images/pressLogos.png" alt="Awards Badges Img" />
+          </div>
+
+      
+        </div>
+        </div>
+        </div>
      );
 }
 
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -4,7 +4,16 @@ import React from 'react';
 function Hero() {
     return ( 
 
-        <h1>Hero Page</h1>
+        <div className="container mb-5 p-5">
+            <div className='row text-center' >
+
+                <img src="media/images/homeHero.png" alt="Hero Img" className="mb-5" />
+                <h1>Invest in Everything</h1>
+                <p>Buy stocks, crypto, ETFs, and more, all in one app.</p>
+                <button className=' p-2 btn btn-primary fs-5  ' style={{width:"20%" , margin: "auto", }}>Signup</button>
+           
+        </div>
+        </div>
      );
 }
 
