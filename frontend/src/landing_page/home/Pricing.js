37dbca5617056209d17b6da0cac141ebc9ae1a97
Shivamkadam692 <shivam.123kadam@gmail.com>
2025-11-26 15:57:26 +0530

    created stat page

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -4,7 +4,35 @@ import React from 'react';
 function Pricing() {
     return ( 
 
-        <h1>Pricing Page</h1>
+      <div className="container mt-5" >
+         <div className="row">
+     
+         <div className="col-4">
+            
+
+            <h1 className='mb-4'>Trust with confidence</h1>
+           
+            <p className='mb-2'>We are proud to be the largest broker in India, serving millions of customers with top-notch services and a wide range of investment options.</p>
+      <div className=''>
+                <a href='#' className=' 'style={{textDecoration:"none"}}>Explore Our Product</a>
+              
+                </div>
+
+      
+        </div>
+        <div className='col-2'></div>
+        
+                <div className="col-6">
+
+                    <div >
+
+                    </div>
+
+              
+        </div>
+        </div>
+        
+        </div>
      );
 }
 
--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -4,7 +4,44 @@ import React from 'react';
 function Stats() {
     return ( 
 
-        <h1>Stats Page</h1>
+     
+     <div className="container mt-5" >
+         <div className="row">
+     
+         <div className="col-6">
+            
+
+            <h1 className='mb-4'>Trust with confidence</h1>
+              <h2>Customer-first Always</h2>
+            <p className='mb-2'>We are proud to be the largest broker in India, serving millions of customers with top-notch services and a wide range of investment options.</p>
+      
+              <h2>Customer-first Always</h2>
+            <p className='mb-2'>We are proud to be the largest broker in India, serving millions of customers with top-notch services and a wide range of investment options.</p>
+      
+              <h2>Customer-first Always</h2>
+            <p className='mb-2'>We are proud to be the largest broker in India, serving millions of customers with top-notch services and a wide range of investment options.</p>
+      
+              <h2>Customer-first Always</h2>
+            <p className='mb-2'>We are proud to be the largest broker in India, serving millions of customers with top-notch services and a wide range of investment options.</p>
+      
+            
+
+          
+
+      
+        </div>
+        
+                <div className="col-6">
+
+                <img src="media/images/ecosystem.png" alt="Awards Img" style={{width:"75%"}} />
+                <div className='text-center'>
+                <a href='#' className='p-5 'style={{textDecoration:"none"}}>Explore Our Product</a>
+                <a href='#'style={{textDecoration:"none"}}>Try Kite</a>
+                </div>
+        </div>
+        </div>
+        
+        </div>
      );
 }
 
