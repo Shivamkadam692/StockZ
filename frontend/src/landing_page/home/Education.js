ad22e5a3ed2cb8844ce5d53bf1d37eecd9fa72c8
Shivamkadam692 <shivam.123kadam@gmail.com>
2025-11-27 12:18:40 +0530

    created home component

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -4,7 +4,47 @@ import React from 'react';
 function Education() {
     return ( 
 
-        <h1>Education Page</h1>
+            <div className="container mt-5" >
+         <div className="row">
+
+                 <div className='col-6'>
+
+
+                    <img src="media/images/education.svg" alt="Awards Img" style={{width:"75%"}} />
+
+
+
+            
+        </div>
+        
+        
+     
+         <div className="col-6">
+            
+
+            <h1 className='mb-4'>Unbeatable pricing</h1>
+           
+            <p className='mb-2'>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+      <div className=''>
+                <a href='#' className=' 'style={{textDecoration:"none"}}>Explore Our Product</a>
+              
+                </div>
+            <br />
+           
+            <p className='mb-2'>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+      <div className=''>
+                <a href='#' className=' 'style={{textDecoration:"none"}}>Explore Our Product</a>
+              
+                </div>
+
+      
+        </div>
+   
+              
+        </div>
+        </div>
+   
+
      );
 }
 
--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -4,15 +4,15 @@ import React from 'react';
 function Pricing() {
     return ( 
 
-      <div className="container mt-5" >
+      <div className="container mt-5 mb-5" >
          <div className="row">
      
          <div className="col-4">
             
 
-            <h1 className='mb-4'>Trust with confidence</h1>
+            <h1 className='mb-4'>Unbeatable pricing</h1>
            
-            <p className='mb-2'>We are proud to be the largest broker in India, serving millions of customers with top-notch services and a wide range of investment options.</p>
+            <p className='mb-2'>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
       <div className=''>
                 <a href='#' className=' 'style={{textDecoration:"none"}}>Explore Our Product</a>
               
@@ -22,12 +22,27 @@ function Pricing() {
         </div>
         <div className='col-2'></div>
         
-                <div className="col-6">
+                <div className="col-6 mb-5">
 
-                    <div >
+                    <div className='row'>
+
+                           <div className=' text-center col p-5 ml-2' style={{border:"solid black 2px", borderRadius:"10px"}}>
+
+                        <h1>0</h1>
+                        <p>Account Opening Fee</p>
+
+                    </div>
+                       <div  className=' text-center col p-5 ' style={{border:"solid black 2px", borderRadius:"10px"}}>
+
+                        <h1>20</h1>
+                        <p>Account Opening Fee</p>
 
                     </div>
 
+
+
+                    </div>
+                  
               
         </div>
         </div>
