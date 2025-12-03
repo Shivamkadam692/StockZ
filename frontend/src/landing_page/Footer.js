cc0ba3a8e4c22e954975e95401ec6c6d966d5de4
Shivamkadam692 <shivam.123kadam@gmail.com>
2025-12-03 20:55:06 +0530

    footer started

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -3,7 +3,15 @@ import React from 'react';
 function Footer() {
     return (  
 
-        <h1>Footer Page</h1>   
+     <div>
+
+<footer class="bg-light text-center text-lg-start">
+  <div class="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+    © 2024 Copyright:
+    <a class="text-dark" href="https://example.com/"> </a>
+  </div>
+</footer>
+     </div>
     );
 }
 
