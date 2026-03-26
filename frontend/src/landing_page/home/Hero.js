bbd56c1f751e62e6319f921e7540a2ad58aab626
Shivamkadam692 <shivam.123kadam@gmail.com>
2026-03-26 22:34:16 +0530

    Update hero heading to be more descriptive

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
             <div className='row text-center' >
 
                 <img src="media/images/homeHero.png" alt="Hero Img" className="mb-5" />
-                <h1>Invest in Everything</h1>
+                <h1>Invest in Everything that you have</h1>
                 <p>Buy stocks, crypto, ETFs, and more, all in one app.</p>
                 <button className=' p-2 btn btn-primary fs-5  ' style={{width:"20%" , margin: "auto", }}>Signup</button>
            
